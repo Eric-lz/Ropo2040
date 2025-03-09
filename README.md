bef4c68c4bcfe2a8ca120b747300fe80f96d6181
Eric Peracchi Pisoni <ericperacchi@gmail.com>
2025-03-08 21:59:33 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Project Overview
+
+This project involves taking a broken robot vacuum cleaner and rewiring all its sensors and motors into an external microcontroller. The goal is to repurpose the hardware and control it using a Raspberry Pi Pico RP2040 running FreeRTOS.
+
+# Hardware
+
+### **Microcontroller**: [RP2040-Zero](https://www.waveshare.com/wiki/RP2040-Zero)
+### **Sensors and Motors**: Salvaged from a very old [Ropo Glass](https://web.archive.org/web/20150526031732/http://www.ropo.com.br/) robot vacuum
+- 2 brushed DC motors with encoders
+- Vacuum and brush motors *(I do not plan to use these)*
+- Weight-on-Wheels switch
+- 4 IR cliff sensors
+- 5 IR distance sensors
+- Bump sensors
+
+# Software
+
+As mentioned above, the RP2040 is running FreeRTOS.
+I'm still working out the folder structure and CMake.
+
+The FreeRTOS kernel is included as a submodule, so make sure to clone the repository with the `--recurse-submodules` flag, or if you've already cloned it, use `git submodule init` and `git submodule update`.
+
+# Current status
+
+This is the first commit, currently the code is just some basic FreeRTOS functionality test on the RP2040.
+
+# Future Plans*
+
+- Integrate all sensors and motors with the RP2040-Zero
+- Develop control algorithms for autonomous operation
+- Implement additional features and improvements
+- Create a custom PCB
+
+_*This is just a hobby project that I'm working on for fun, so I expect to abandon it unfinished at any time lol_
---
+++ Ropo2040.c
@@ -0,0 +1,100 @@
+// FreeRTOS
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+
+// Pico
+#include "pico/stdlib.h"
+#include "pico/multicore.h"
+
+#include <stdio.h>
+
+// Core mask: 0b01 -> Core 0, 0b10 -> Core 1
+#define CORE_0_MASK (1 << 0)
+#define CORE_1_MASK (1 << 1)
+
+// LED pin
+const uint LED_PIN = 14;
+
+// Create queue handle (used for communication between tasks)
+static QueueHandle_t xQueue = NULL;
+
+void led_task(void *pvParameters){
+    printf("LED_Task started!\n");
+
+    // Setup GPIO
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    // Blink LED
+    while (true) {
+        gpio_put(LED_PIN, 1);
+        vTaskDelay(100);
+        gpio_put(LED_PIN, 0);
+        vTaskDelay(100);
+    }
+}
+
+// Every second, read from queue and print
+void print_task(void *pvParameters){
+    printf("Print_Task started!\n");
+
+    int num;
+
+    while(1){
+        int ret = xQueueReceive(xQueue, &num, pdMS_TO_TICKS(1000));
+
+        if(ret == pdPASS){
+            printf("Number recv: %d, Core %d\n", num, get_core_num());
+        }
+        else{
+            printf("Queue empty!\n");
+        }
+
+        vTaskDelay(1000);
+    }
+}
+
+// Every second, generate new number and add to queue
+void rng_task(void *pvParameters){
+    printf("Print_Task started!\n");
+
+    int num = 0;
+
+    while(1){
+        num++;
+
+        int ret = xQueueSend(xQueue, &num, pdMS_TO_TICKS(1000));
+        printf("Number sent: %d, Core %d\n", num, get_core_num());
+
+        if(ret != pdPASS){
+            printf("Queue full!\n");
+        }
+
+        vTaskDelay(1000);
+    }
+}
+
+int main(){
+    stdio_init_all();
+
+    // Init queue
+    xQueue = xQueueCreate(1, sizeof(int));
+
+    // Task handles
+    TaskHandle_t rng_task_handle;
+    TaskHandle_t print_task_handle;
+
+    // Start tasks
+    xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
+    xTaskCreate(rng_task, "RNG_Task", 256, NULL, 1, &rng_task_handle);
+    xTaskCreate(print_task, "Print_Task", 256, NULL, 1, &print_task_handle);
+    
+    // Pin task to core (has to be done before starting scheduler)
+    // If task is not pinned, the scheduler will assign it to a core
+    vTaskCoreAffinitySet(rng_task_handle, CORE_0_MASK);
+    vTaskCoreAffinitySet(print_task_handle, CORE_1_MASK);
+
+    // Start scheduler
+    vTaskStartScheduler();
+}
