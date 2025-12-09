a0d9fdbcb47c00d79a0c1adb99b9188f0e457a74
Eric Peracchi Pisoni <ericperacchi@gmail.com>
2025-12-09 02:07:03 -0300

    Removed debug/unused code

--- src/Ropo2040.cpp
+++ src/Ropo2040.cpp
@@ -43,14 +43,13 @@ int main(){
 
     // Motor
     xTaskCreate(const_speed_task, "ConstSpeed_Task", 256, (void*) &xQEncoder, 5, NULL);
-    // xTaskCreate(oneturn_task, "OneTurn_Task", 256, (void*) &xQEncoder, 5, NULL);
+    // xTaskCreate(oneturnpid_task, "OneTurn_Task", 256, (void*) &xQEncoder, 5, NULL);
 
     // INA219 Current sensor
     // xTaskCreate(print_current, "INA219_Task", 256, NULL, 1, NULL);
     
     // Debug tasks
     xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
-    // xTaskCreate(pwm_task, "PWM_Task", 256, NULL, 1, NULL);
     // xTaskCreate(rng_task, "RNG_Task", 256, (void*) xQueue, 1, &rng_task_handle);
     // xTaskCreate(print_task, "Print_Task", 256, (void*) xQueue, 1, &print_task_handle);
     // xTaskCreate(pulse_pin, "Pulse_Pin", 256, NULL, 4, NULL);
